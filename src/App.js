d53370541f33f41ea4ca2fdaaaf772333c2d8880
kietNguyen <nguyentuankietmd2003@gmail.com>
2024-03-15 03:40:09 +0700

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from "react";
+import FaceImage from "./FaceImage";
+import GlassesList from "./GlassesList";
+import "./appp.css";
 
 function App() {
+  const [selectedGlasses, setSelectedGlasses] = useState(null);
+
+  const handleGlassesClick = (glassesUrl) => {
+    setSelectedGlasses(glassesUrl);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app appp  d-flex align-items-center">
+      <FaceImage selectedGlasses={selectedGlasses} />
+      <GlassesList onGlassesClick={handleGlassesClick} />
     </div>
   );
 }
